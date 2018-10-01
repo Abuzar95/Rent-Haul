cc7f97bfc28b55e5a25bb7a40184a2ac13b9969e
Abuzar Manzoor <abuzarmanzoor@hotmail.com>
2018-10-01 13:02:05 +0500

    Initial Commit

---
+++ Rent Haul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rent Haul
+//
+//  Created by Abuzar Manzoor on 01/10/2018.
+//  Copyright © 2018 AbuzarManzoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
